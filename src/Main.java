30ebc9bfa78b70009ebee28f6f2f2433fa37e0e3
MarselMingaripov <bard1812@gmail.com>
2023-01-26 17:52:53 +0300

    выполнил задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+public class Main {
+    public static int countWords(List<String> list, String word) {
+        return Collections.frequency(list, word);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input:");
+        String str = scanner.nextLine();
+        List<String> stringList = new ArrayList<>();
+        for (String string : str.split(" ")) {
+            stringList.add(string);
+        }
+        System.out.println("В тексте " + stringList.size() + " слов");
+
+        Map<String, Long> frequency =
+                stringList.stream().collect(Collectors.groupingBy(
+                        Function.identity(), Collectors.counting()));
+        frequency.values().stream().sorted(Long::compareTo);
+
+        for (Map.Entry<String, Long> stringLongEntry : frequency.entrySet()) {
+            System.out.println(stringLongEntry);
+        }
     }
 }
